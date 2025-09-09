cfa840a19c504a438248696ed026bc93f774fa51
AlexKT1022 <alexkt1022@gmail.com>
2025-09-09 10:11:11 -0400

    configured app.js, moved error handling middleware to a new file

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import errorHandler from '#middleware/errorHandler';
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use(errorHandler);
