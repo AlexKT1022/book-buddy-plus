6816c82f753354dc55b334e8995f3ed73299cb3d
AlexKT1022 <alexkt1022@gmail.com>
2025-09-09 11:34:23 -0400

    added basic api routes
    Co-authored-by: Amador Baggerly <abaggerly@users.noreply.github.com>
    Co-authored-by: Eduardo310-mx <Eduardo310-mx@users.noreply.github.com>
    Co-authored-by: nvila-iso <nvila-iso@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
-import errorHandler from '#middleware/errorHandler';
 import express from 'express';
 import morgan from 'morgan';
+
+import booksRouter from '#api/booksRouter';
+import reservationsRouter from '#api/reservationsRouter';
+import usersRouter from '#api/usersRouter';
+import errorHandler from '#middleware/errorHandler';
 
 const app = express();
 
@@ -9,4 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 
+app.use('/books', booksRouter);
+app.use('/users', usersRouter);
+app.use('/reservations', reservationsRouter);
+
 app.use(errorHandler);
+
+export default app;
